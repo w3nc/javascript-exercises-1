8d51df7617b65a4b4fab7b6e4cf8a4c922c818b1
Wenc <199257164+w3nc@users.noreply.github.com>
2026-07-20 20:33:32 +0800

    Re-created the snippets in example.js to finish the activity

--- foundations/05_joinStrings/joinStrings.js
+++ foundations/05_joinStrings/joinStrings.js
@@ -9,17 +9,22 @@
 
 // Add your code right below, good luck!
 
-
-
-
+let firstName = "Carlos";
+let lastName = "Stevenson";
+let thisYear = 1965;
+let birthYear = 1947;
+let fullName = `${firstName} ${lastName}`;
+let age = thisYear - birthYear;
+let greeting =
+  "Hello! My name is " + fullName + " and I am " + age + " years old.";
 
 // Do not change this
 module.exports = {
-  firstName: typeof firstName === 'undefined' ? undefined : firstName,
-  lastName: typeof lastName === 'undefined' ? undefined : lastName,
-  thisYear: typeof thisYear === 'undefined' ? undefined : thisYear,
-  birthYear: typeof birthYear === 'undefined' ? undefined : birthYear,
-  greeting: typeof greeting === 'undefined' ? undefined : greeting,
-  fullName: typeof fullName === 'undefined' ? undefined : fullName,
-  age: typeof age === 'undefined' ? undefined : age
-}
+  firstName: typeof firstName === "undefined" ? undefined : firstName,
+  lastName: typeof lastName === "undefined" ? undefined : lastName,
+  thisYear: typeof thisYear === "undefined" ? undefined : thisYear,
+  birthYear: typeof birthYear === "undefined" ? undefined : birthYear,
+  greeting: typeof greeting === "undefined" ? undefined : greeting,
+  fullName: typeof fullName === "undefined" ? undefined : fullName,
+  age: typeof age === "undefined" ? undefined : age,
+};
