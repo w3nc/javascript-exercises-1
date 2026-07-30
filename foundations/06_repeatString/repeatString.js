12c3d38f132137cb2f8407bc20d2ad8444c8ba6b
Wenc <199257164+w3nc@users.noreply.github.com>
2026-07-31 01:37:00 +0800

    Added my code/solution inside the given function

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function (word, num) {
 
+if(num < 0)return 'ERROR';
+
+let result = "";
+
+for(let i = 0; i < num; i++){
+    result += word;
+}
+
+return result;
 };
 
 // Do not edit below this line
