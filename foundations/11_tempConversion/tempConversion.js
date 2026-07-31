bf4feb1192454ebb3ec145d1b639955804547b70
Wenc <199257164+w3nc@users.noreply.github.com>
2026-08-01 01:58:02 +0800

    Added code into those two functions provided

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,11 +1,29 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (fah) {
+  let cel = ((fah - 32) * 5) / 9;
+
+  if (!Number.isInteger(cel)) {
+    // let rounded = Math.round(cel * 10) / 10;
+    let fixed = cel.toFixed(1);
+    return Number(fixed);
+  }
+
+  return cel;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (cel) {
+  let fah = (cel * 9) / 5 + 32;
+
+  if (!Number.isInteger(fah)) {
+    // let rounded = Math.round(fah * 10) / 10;
+    let fixed = fah.toFixed(1);
+    return Number(fixed);
+  }
+
+  return fah;
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
