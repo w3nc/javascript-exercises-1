88799dd69c2e0ca64fe3fb3c0ae837d44cb510dc
Wenc <199257164+w3nc@users.noreply.github.com>
2026-07-31 23:48:31 +0800

    Added an input validation and loop inside the function

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
+const sumAll = function (num1, num2) {
+  const isValid = (n) => Number.isInteger(n) && n > 0;
 
+  if (!isValid(num1) || !isValid(num2)) {
+    return "ERROR";
+  }
+
+  let sum = 0;
+
+  const start = Math.min(num1, num2);
+  const end = Math.max(num1, num2);
+
+  for (let i = start; i <= end; i++) {
+    sum += i;
+  }
+
+  return sum;
 };
 
 // Do not edit below this line
