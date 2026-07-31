5ce4f30f5973a63a3e9317c30d7afa18b85b6be8
Wenc <199257164+w3nc@users.noreply.github.com>
2026-08-01 00:58:24 +0800

    Added a simple conditional statement to the function

--- foundations/10_leapYears/leapYears.js
+++ foundations/10_leapYears/leapYears.js
@@ -1,5 +1,12 @@
-const leapYears = function() {
-
+const leapYears = function (year) {
+  let isLeapYear;
+  if (year % 400 === 0 || (year % 4 === 0 && !(year % 100 === 0))) {
+    isLeapYear = true;
+    return isLeapYear;
+  } else {
+    isLeapYear = false;
+    return isLeapYear;
+  }
 };
 
 // Do not edit below this line
