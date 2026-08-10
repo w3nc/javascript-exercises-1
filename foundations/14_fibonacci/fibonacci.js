672deca4027da1783ceab56343c547c653e68e07
w3nc <199257164+w3nc@users.noreply.github.com>
2026-08-11 06:17:05 +0800

    Added some codes inside fibonacci function

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function (num) {
+  num = Number(num);
 
+  if (num < 0) return "OOPS";
+
+  let a = 0; // previous-previous
+  let b = 1; // previous
+
+  for (let i = 0; i < num; i++) {
+    [a, b] = [b, a + b]; // step forward one position
+  }
+
+  return a;
 };
 
 // Do not edit below this line
