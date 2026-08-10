65add521b594f6f59bfb0d0ac5b2cc376993d1c2
w3nc <199257164+w3nc@users.noreply.github.com>
2026-08-11 04:49:33 +0800

    Added some codes inside palindromes function

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  const cleaned = str.toLowerCase().replace(/[^a-z0-9]/g, "");
+  const reversed = cleaned.split("").reverse().join("");
 
+  return cleaned === reversed;
 };
 
 // Do not edit below this line
